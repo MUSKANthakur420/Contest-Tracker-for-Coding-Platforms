ac6484e1ec8c0c3792319ac32b8f9b0c7ae46afd
Muskan Singh <420royalrajput50806@gmail.com>
2026-09-20 02:41:22 +0530

    update production backend url

--- contestTrackerFrontend/src/utils/axios.js
+++ contestTrackerFrontend/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const instance=axios.create({
-    baseURL:"http://localhost:8000",
-    withCredentials:true
-})
+const instance = axios.create({
+    baseURL: "https://contest-tracker-for-coding-platforms-1.onrender.com",
+    withCredentials: true
+});
 export default instance;
